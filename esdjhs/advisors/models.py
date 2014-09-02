50ea30acb11ad8bb71922c29d89e61c975fca6e3
Brent Payne <brent.payne@brightscope.com>
2014-09-02 16:07:08 -0700

    adding company and advisor models

--- esdjhs/advisors/models.py
+++ esdjhs/advisors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=256)
+    doing_business_as_name = models.CharField(max_length=256)
+    full_address = models.CharField(max_length=256)
+
+
+class Advisor(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    company = models.ForeignKey(Company)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
