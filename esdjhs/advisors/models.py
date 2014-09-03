6a34c0d991c6ef6dc5e62933a0edb0dd5dd07df0
Brent Payne <brent.payne@brightscope.com>
2014-09-03 11:52:10 -0700

    removing doing_business_as name

--- esdjhs/advisors/models.py
+++ esdjhs/advisors/models.py
@@ -4,13 +4,13 @@ from django.db import models
 
 class Company(models.Model):
     company_name = models.CharField(max_length=256)
-    doing_business_as_name = models.CharField(max_length=256)
     full_address = models.CharField(max_length=256)
 
 
 class Advisor(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+    crd = models.IntegerField()
     company = models.ForeignKey(Company)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
