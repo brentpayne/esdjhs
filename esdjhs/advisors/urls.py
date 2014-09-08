d5f6425a8227885119a8208555530a4c234df699
Brent Payne <brent.payne@brightscope.com>
2014-09-08 14:20:06 -0700

    stubbed out first name search

--- esdjhs/advisors/urls.py
+++ esdjhs/advisors/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^<first_name>/$', views.first_name_search),
+    url(r'^(?P<first_name>\S+)/$', views.first_name_search),
 ]
