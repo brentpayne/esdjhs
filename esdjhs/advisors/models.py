5d812b7239153a4c13fc09149b1cc07c6e94ac56
Brent Payne <brent.payne@brightscope.com>
2014-09-09 16:04:02 -0700

    adding first name search results to the view

--- esdjhs/advisors/models.py
+++ esdjhs/advisors/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Company(models.Model):
     company_name = models.CharField(max_length=256)
--- esdjhs/advisors/views.py
+++ esdjhs/advisors/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from advisors.search_indexes import AdvisorIndex
+
 
 def first_name_search(request, first_name):
     context = dict(first_name=first_name)
+    context['advisors'] = AdvisorIndex.objects.filter(first_name=first_name)[:5]
     return render(request, 'advisors/first_name_search.html', context)
